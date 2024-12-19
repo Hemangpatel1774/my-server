394d7083149361a2808a1d5f36ab6e0278b03295
Hemang Patel <patelhemang1774@gmail.com>
2024-12-19 11:04:25 +0530

    try catch on bdcon..!

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ dotEnv.config()
 const port = process.env.PORT;
 
 server.use((req, res, next) => {
+    try{
     mongoose.connect(process.env.DBURl)
         .then(() => {
             next();
             console.log("db Connect successfully...!");
         })
-})
+    }catch(e){
+        console.log(e);
+        
+    }    
+    })
 
 server.get("/", (req, res) => {
     res.send("Server running...!")
