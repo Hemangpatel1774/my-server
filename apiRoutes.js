d085ba64fe68aa4a060c93d408e68666e03ba0ae
Hemang Patel <patelhemang1774@gmail.com>
2024-12-19 11:23:29 +0530

    Send otp rout try catch

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const magicMaster = require("./Models/magicModel");
-const generateOtp = require("./functions/generateOtp");
-const sendOtp = require("./functions/sendOtp");
+const sendOtp = require("./functions/sendOtp")
+const generateOtp = require("./functions/generateOtp")
 const apiRoutes = express.Router();
 apiRoutes.get("/test",(req,res)=>{
     res.send("apiRoutes here /test")
@@ -9,8 +9,8 @@ apiRoutes.get("/test",(req,res)=>{
 apiRoutes.get("/sendOtp/:email", async (req,res)=>{
     try{
         const email= req.params.email;
-        res.send("otp sent successfully...!"+email);
-    const otp = generateOtp();
+        const otp = generateOtp();
+        res.send("otp sent successfully...!"+otp);
     sendOtp(email,otp);
     console.log(otp);
     const newData = magicMaster({email , otp})
