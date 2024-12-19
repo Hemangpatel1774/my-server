d070454957d0b885e35982c9f4fb1beedcbfee29
Hemang Patel <patelhemang1774@gmail.com>
2024-12-19 16:29:55 +0530

    change sendMail() para

--- apiRoutes.js
+++ apiRoutes.js
@@ -3,29 +3,25 @@ const magicMaster = require("./Models/magicModel");
 const sendOtpNo = require("./functions/sendOtp")
 const generateOtp = require("./functions/generateOtp")
 const apiRoutes = express.Router();
-apiRoutes.get("/test",(req,res)=>{
+apiRoutes.get("/test", (req, res) => {
     res.send("apiRoutes here /test")
 });
-apiRoutes.post("/sendOtp", async (req,res)=>{
-    try{
-        const email= req.body.email;
+apiRoutes.post("/sendOtp", async (req, res) => {
+    try {
+        const email = req.body.email;
         const otp = generateOtp();
-        try{
-        let a =await sendOtpNo(email,otp);
-        res.send("otp sent successfully...!"+a);
-        }catch(e){
-            res.send(e.message)
-        }
-        const newData = magicMaster({email , otp})
-    await newData.save()
-    }catch(e){
+        let a = sendOtpNo(email, otp);
+        res.send("otp sent successfully...!");
+        const newData = magicMaster({ email, otp })
+        await newData.save()
+    } catch (e) {
         res.send(e);
     }
 });
-apiRoutes.get("/verifyOtp",(req,res)=>{
+apiRoutes.get("/verifyOtp", (req, res) => {
     res.send("apiRoutes here /test")
 });
-apiRoutes.post("/getdata",(req,res)=>{
+apiRoutes.post("/getdata", (req, res) => {
     res.send("")
 })
 
