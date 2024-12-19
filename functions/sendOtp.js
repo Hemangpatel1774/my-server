0ed8c691a94d32affb492a29119869076a167230
Hemang Patel <patelhemang1774@gmail.com>
2024-12-19 11:55:02 +0530

    Send otp rout try catch

--- functions/sendOtp.js
+++ functions/sendOtp.js
@@ -4,32 +4,28 @@ dotenv.config();
 
 
 const sendOtp = (userEmail, otp) => {
-    try {
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.MAILUSERNAME,
-                pass: process.env.MAILPASS,
-            }
-        });
-        let mailOptions = {
-            from: '15hemagn1774@gmail.com',
-            to: userEmail,
-            subject: 'Interview Authentication',
-            html: '<h1>Welcome to nits</h1><br><br>Your OTP is ' + otp,
-        };
+    return "hi";
+    // let transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: process.env.MAILUSERNAME,
+    //         pass: process.env.MAILPASS,
+    //     }
+    // });
+    // let mailOptions = {
+    //     from: '15hemagn1774@gmail.com',
+    //     to: userEmail,
+    //     subject: 'Interview Authentication',
+    //     html: '<h1>Welcome to nits</h1><br><br>Your OTP is ' + otp,
+    // };
 
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                return "error"
-            } else {
-                return "otp send to " + userEmail
-            }
-        });
-        return "ok"
-    }catch(e){
-        return e
-    }
+    // transporter.sendMail(mailOptions, function (error, info) {
+    //     if (error) {
+    //         return "error"
+    //     } else {
+    //         return "otp send to " + userEmail
+    //     }
+    // });
 }
 
 module.exports = sendOtp;
